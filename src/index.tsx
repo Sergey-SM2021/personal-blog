9e5d97ae63221fe5609fb0e0aa78b2a67dda7e64
Sergey-SM2021 <84789763+Sergey-SM2021@users.noreply.github.com>
2022-06-01 15:23:15 +0300

    init: Added required dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { App } from './App'
+import reportWebVitals from './reportWebVitals'
+import { GlobalStyle } from './Global.style'
+import { Provider } from 'react-redux'
+import { store } from './store'
+import { BrowserRouter } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <GlobalStyle />
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
