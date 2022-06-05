3a7577cbe9aa5e786b7d6c01e92b69a2dd9b7f37
Sergey-SM2021 <84789763+Sergey-SM2021@users.noreply.github.com>
2022-06-05 10:18:01 +0300

    new

--- src/components/Main/Main.tsx
+++
@@ -1,5 +0,0 @@
-import { MainWrapper } from "./Main.style"
-
-export const Main = () => {
-    return (<MainWrapper>Main</MainWrapper>)
-}
--- src/components/Sider/Sider.style.ts
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components"
-
-export const SiderWrapper = styled.div`
-    background-color:#345;
-    position:absolute;
-    top:0;
-    left:0;
-    width:200px;
-`
